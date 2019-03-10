4b236ad0d4ba96be9e64d1a85aca97e2324a9cc4
ifaist0s <ifaist0s@users.noreply.github.com>
2019-03-11 01:50:56 +0200

    Logfile support

--- vesta-server-ssl-cert.sh
+++ vesta-server-ssl-cert.sh
@@ -8,7 +8,8 @@
 # Notification email parameters
 mailto='CHANGE THIS TO YOUR EMAIL ADDRESS'
 mailsub="Server SSL Renewal: "$(hostname -f)
-
+LOGFILE=vesta-server-ssl-cert.log
+ 
 # Set the paths of SSL certificates to check
 path2le=/home/admin/conf/web
 path2ve=/usr/local/vesta/ssl
@@ -27,58 +28,58 @@ if ! cmp --silent $LEcrt $VEcrt
 then
 	echo CERTIFICATES DIFFERENT - UPDATING
 	# Copy certificates for VESTA use
-	cp --backup $LEcrt $VEcrt
-	cp --backup $LEkey $VEkey
-	cp --backup $LEpem $VEpem
+	cp --backup $LEcrt $VEcrt > $LOGFILE 2>&1
+	cp --backup $LEkey $VEkey >> $LOGFILE 2>&1
+	cp --backup $LEpem $VEpem >> $LOGFILE 2>&1
 	# Set owner and permissions for mail user
-	chown root:mail $VEcrt $VEkey $VEpem
-	chmod 640 $VEcrt $VEkey $VEpem
+	chown root:mail $VEcrt $VEkey $VEpem >> $LOGFILE 2>&1
+	chmod 640 $VEcrt $VEkey $VEpem >> $LOGFILE 2>&1
 	
 	# Copy certificates for MySQL use
-	cp $VEcrt $path2sq
-	cp $VEkey $path2sq
-	cp $VEpem $path2sq
+	cp $VEcrt $path2sq >> $LOGFILE 2>&1
+	cp $VEkey $path2sq >> $LOGFILE 2>&1
+	cp $VEpem $path2sq >> $LOGFILE 2>&1
 	# Set owner and permissions for mysql user
-	chown root:mysql $path2sq/certificate.*
-	chmod 640 $path2sq/certificate.*
+	chown root:mysql $path2sq/certificate.* >> $LOGFILE 2>&1
+	chmod 640 $path2sq/certificate.* >> $LOGFILE 2>&1
 
 	# Restart services that depend on these certificates
 	case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
 		Debian)
 			case $(lsb_release -s -r) in
 				9.8)
-					systemctl restart vesta exim4 dovecot vsftpd mysql
+					systemctl restart vesta exim4 dovecot vsftpd mysql >> $LOGFILE 2>&1
 					;;
 				*)
-					echo UNKNOWN DEBIAN RELEASE. Restart services manualy.
+					echo UNKNOWN DEBIAN RELEASE. Restart services manualy. >> $LOGFILE 2>&1
 					;;
 			esac
 			;;
 		Ubuntu)
 			case $(lsb_release -s -r) in
 				18.04)
-					systemctl restart vesta exim4 dovecot vsftpd mysql
+					systemctl restart vesta exim4 dovecot vsftpd mysql >> $LOGFILE 2>&1
 					;;
 				16.04)
-					systemctl restart vesta exim4 dovecot vsftpd mysql
+					systemctl restart vesta exim4 dovecot vsftpd mysql >> $LOGFILE 2>&1
 					;;
 				14.04)
-					/usr/sbin/service vesta restart
-					/usr/sbin/service exim4 restart
-					/usr/sbin/service mysql restart
-					/usr/bin/doveadm reload
-					/sbin/initctl restart vsftpd
+					/usr/sbin/service vesta restart >> $LOGFILE 2>&1
+					/usr/sbin/service exim4 restart >> $LOGFILE 2>&1
+					/usr/sbin/service mysql restart >> $LOGFILE 2>&1
+					/usr/bin/doveadm reload >> $LOGFILE 2>&1
+					/sbin/initctl restart vsftpd >> $LOGFILE 2>&1
 					;;
 				*)
-					echo UNKNOWN UBUNTU RELEASE. Restart services manualy.
+					echo UNKNOWN UBUNTU RELEASE. Restart services manualy. >> $LOGFILE 2>&1
 					;;
 			esac
 			;;
 		*)
-			echo UNKNOWN OS. Restart services manualy.
+			echo UNKNOWN OS. Restart services manualy. >> $LOGFILE 2>&1
 			;;
 	esac
 	
 	# Notify
-	which mail > /dev/null 2>&1 && echo "The server certificate at "$(hostname -f)" has been renewed successfully :)" | mail -s "$mailsub" "$mailto"
+	which mail > /dev/null 2>&1 && echo "The server certificate at "$(hostname -f)" has been renewed successfully :)" | mail -s "$mailsub" -a $LOGFILE "$mailto"
 fi
